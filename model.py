9f69ff8a97ea9e3d764ce7600698a0cc540e6b56
hhio618 <hhio618@gmail.com>
2018-07-21 00:21:50 +0430

    Small test

--- model.py
+++ model.py
@@ -20,13 +20,14 @@ def run(item, n_epochs, lr):
                                                              n_features=n_features)
     print(X_train.shape)
     print(y_train.shape)
+
     scaler = None #joblib.load("data/scalers/%s.scaler" % item)
     model = ComplexModel(item, input_shape=X_train.shape[1:],output_size=output_size,
                          scaler=scaler,
                          n_cells=50,
                          n_lags=n_lags,
                          n_features=n_features)
-    model.train(X_train, y_train, lr=lr,
+    model.train(X_train[:100], y_train[:100], lr=lr,
                 validation_split=0.2, n_epochs=n_epochs, batch_size=8)
     model.predict(X_test, y_test)
     print("Base block for prediction ready!")
