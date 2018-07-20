bb8721bc553b9f3f2ad9a8c54eb57307915aebbf
hhio618 <hhio618@gmail.com>
2018-07-21 01:24:16 +0430

    Small test

--- base/complex_block.py
+++ base/complex_block.py
@@ -110,9 +110,9 @@ class ComplexModel(object):
         yhat = self.model.predict(X_test)
 
         # # X_test for forecasting
-        inv_yhat = yhat.reshape((yhat.size(), 1))
+        inv_yhat = np.ravel(yhat)
         # invert scaling for actual
-        inv_y = y_test.reshape((y_test.size(), 1))
+        inv_y = np.ravel(y_test)
         # calculate RMSE
         rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
         mae = mean_absolute_error(inv_y, inv_yhat)
